7bcd38ea84d24fa5e3f127cabf83afba28392fc7
shravanaa <shravanaa27@gmail.com>
2023-02-15 11:04:40 +0530

    third Commit

--- chaincode/land/index.js
+++ chaincode/land/index.js
@@ -5,6 +5,8 @@
 'use strict';
 
 const LandContract = require('./lib/land-contract');
+const oldLandRecord = require('./lib/oldLand');
 
 module.exports.LandContract = LandContract;
-module.exports.contracts = [ LandContract ];
+module.exports.oldLandRecord = oldLandRecord;
+module.exports.contracts = [ LandContract , oldLandRecord];
--- chaincode/land/lib/oldLand.js
+++ chaincode/land/lib/oldLand.js
@@ -4,13 +4,12 @@
 
 'use strict';
 
-
 const { Contract } = require('fabric-contract-api');
-const { Client, EventHub } = require('fabric-client');
+//const { Client, EventHub } = require('fabric-client');
 const util = require('util');
+const { LandContract } = require ('./land-contract')
 
-
-class LandRecordChaincode extends Contract {
+class oldLandRecord extends Contract {
 
   async initLedger(ctx) {
     console.info('Instantiate the ledger');
@@ -71,20 +70,30 @@ class LandRecordChaincode extends Contract {
   }
 
   async connectToOldDb(){
-    const MongoClient = require('mongodb').MongoClient;
-    const uri = "mongodb+srv://kbalandregistryproject2023:Login123@cluster0.kadygun.mongodb.net/?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true });
-    client.connect(err => {
+    
+    //fileURLToPath:"/home/shravan/Documents/landReg/oldDbConnect/oldDbconnect.js";
+    
+  const MongoClient = require('mongodb').MongoClient;
+  const uri = "mongodb+srv://kbalandregistryproject2023:Login123@cluster0.kadygun.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect(err => {
     if (err) throw err;
     console.log("Connected successfully to MongoDB in the cloud");
-    const db = client.db("test");
+  
+    const db = client.db("landRegistry");
+    const collection = db.collection("landdetails");
+    collection.find({}).toArray((err, docs) => {
+    if (err) throw err;
+    console.log("Data retrieved from MongoDB:");
+    console.log(docs);
     client.close();
+  });
+  
 });
 
 
-
   }
-
 }
 
-module.exports = LandRecordChaincode;
+
+module.exports = oldLandRecord;
